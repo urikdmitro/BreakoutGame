960f07e70f05827d8287e46ec0281c614f1220d5
Urik Dmitro <urikdmitro@gmail.com>
2023-07-18 01:17:29 +0300

    feat(shader): added methods who checks shader compile status; added Compile method; added default constructor

--- Breakout/src/Renderer/shader.cpp
+++ Breakout/src/Renderer/shader.cpp
@@ -1,31 +1,56 @@
 #include "shader.h"
 #include "cstring"
-#include <exception>
 
 Shader::Shader(const std::string &vertex_source,
                const std::string &fragment_source,
                const std::string &geometry_source) {
 
+  Compile(vertex_source, fragment_source, geometry_source);
+}
+
+Shader::~Shader() { glDeleteProgram(id_); }
+
+// FIXME: horrible piece of shit, need to fix dublication of the code
+unsigned int Shader::Compile(const std::string &vertex_source,
+                             const std::string &fragment_source,
+                             const std::string &geometry_source) {
+
   this->id_ = glCreateProgram();
+  bool is_compiled;
 
   unsigned int vertex_shader_id = CompileShader(vertex_source);
-  glAttachShader(this->id_, vertex_shader_id);
+  is_compiled = CheckShaderCompileErrors(vertex_shader_id);
 
   unsigned int fragment_shader_id = CompileShader(fragment_source);
+  is_compiled = CheckShaderCompileErrors(fragment_shader_id);
+
+  glAttachShader(this->id_, vertex_shader_id);
   glAttachShader(this->id_, fragment_shader_id);
 
   unsigned int geometry_shader_id;
   if (!geometry_source.empty()) {
     geometry_shader_id = CompileShader(geometry_source);
-    glAttachShader(this->id_, geometry_shader_id);
+    is_compiled = CheckShaderCompileErrors(geometry_shader_id);
+
+    if (is_compiled)
+      glAttachShader(this->id_, geometry_shader_id);
   }
 
-  glLinkProgram(this->id_);
+  if (is_compiled)
+    glLinkProgram(this->id_);
 
+  glDetachShader(id_, vertex_shader_id);
   glDeleteShader(vertex_shader_id);
+
+  glDetachShader(id_, fragment_shader_id);
   glDeleteShader(fragment_shader_id);
-  if (!geometry_source.empty())
+
+  if (!geometry_source.empty()) {
+    glDetachShader(id_, geometry_shader_id);
     glDeleteShader(geometry_shader_id);
+  }
+
+  return is_compiled;
 }
 
 unsigned int Shader::CompileShader(const std::string &name) {
@@ -39,4 +64,34 @@ unsigned int Shader::CompileShader(const std::string &name) {
   glCompileShader(shader_id);
 
   return shader_id;
+}
+
+// TODO: methods CheckShaderCompileErrors and CheckShaderProgramCompileErrors
+// have similar implementation, need to fix it
+unsigned int Shader::CheckShaderCompileErrors(unsigned int shader) {
+  int success;
+  char infoLog[1024];
+
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+    // FIXME: add log
+    return 0;
+  }
+
+  return 1;
+}
+
+unsigned int Shader::CheckShaderProgramCompileErrors(unsigned int shader) {
+  int success;
+  char infoLog[1024];
+
+  glGetProgramiv(shader, GL_COMPILE_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+    // FIXME: add log
+    return 0;
+  }
+
+  return 1;
 }
--- Breakout/src/Renderer/shader.h
+++ Breakout/src/Renderer/shader.h
@@ -10,10 +10,19 @@ private:
   unsigned int id_;
 
   unsigned int CompileShader(const std::string &name);
+  unsigned int CheckShaderCompileErrors(unsigned int shader);
+  unsigned int CheckShaderProgramCompileErrors(unsigned int shader);
 
 public:
+  Shader() = default;
   Shader(const std::string &vertex_source, const std::string &fragment_source,
          const std::string &geometry_source);
+
+  ~Shader();
+
+  unsigned int Compile(const std::string &vertex_source,
+                       const std::string &fragment_source,
+                       const std::string &geometry_source);
 
   unsigned int GetId();
   Shader &Use();

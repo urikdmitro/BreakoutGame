80d1382c5a484f5bce3f9de41b0ae6ec40e62822
Urik Dmitro <urikdmitro@gmail.com>
2023-10-08 17:40:54 +0300

    refac(window_glfw): some chore work and refactors

--- Breakout/src/Platform/Linux/GLFW/window_glfw.cpp
+++ Breakout/src/Platform/Linux/GLFW/window_glfw.cpp
@@ -1,23 +1,36 @@
 #include "window_glfw.h"
+#include "log.h"
 
-WindowGLFW::WindowGLFW(const WindowProperties &window_properties) {
-  window_properties_.height = window_properties.height;
-  window_properties_.width = window_properties.width;
-  window_properties_.title = window_properties.title;
+WindowGLFW::WindowGLFW(const WindowProperties &window_properties)
+    : window_properties_(window_properties), window_(nullptr) {
 
   glfwInit();
+
   window_ =
       glfwCreateWindow(window_properties_.width, window_properties_.height,
                        window_properties_.title.c_str(), nullptr, nullptr);
+
+  // TODO: make assertion or macro
+  if (window_ == NULL)
+    LOG_GLOBAL_FATAL("Failed to create GLFW window");
+  else
+    LOG_GLOBAL_INFO("Created GLFW window");
+
+  glfwMakeContextCurrent(window_);
+  glfwSetFramebufferSizeCallback(window_, nullptr);
 }
 
 WindowGLFW::~WindowGLFW() {
   glfwDestroyWindow(window_);
   glfwTerminate();
+  LOG_GLOBAL_INFO("Destroyed GLFW window");
 }
 
 int WindowGLFW::GetWidth() { return window_properties_.width; }
 
 int WindowGLFW::GetHeight() { return window_properties_.height; }
 
-void WindowGLFW::Update() { glfwPollEvents(); }
+void WindowGLFW::Update() {
+  glfwSwapBuffers(window_);
+  glfwPollEvents();
+}
